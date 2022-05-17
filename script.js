a659862717f4bf381abfb186dab9a5c65d6d4ef8
belie369 <katarzyna.derbich@gmail.com>
2022-05-17 11:54:59 +0200

    Added swap function

--- script.js
+++ script.js
@@ -20,8 +20,16 @@ const calculate = () => {
 		});
 };
 
+const swap = () => {
+	const oldValue = currencyOne.value;
+	currencyOne.value = currencyTwo.value;
+	currencyTwo.value = oldValue;
+	calculate();
+};
+
 currencyOne.addEventListener('change', calculate);
 currencyTwo.addEventListener('change', calculate);
 amountOne.addEventListener('input', calculate);
+swapBtn.addEventListener('click', swap);
 
 calculate();
